9b1e7ce94cffc14a6e6de36e1152e41bdb1359b3
Nguyenkien13082001 <kienntgbh190773@fpt.edu.vn>
2024-05-03 22:04:59 +0700

    ted 222

--- src/components/Listpost/Post.js
+++ src/components/Listpost/Post.js
@@ -17,6 +17,8 @@ export default function Post({ getPosts }) {
   const [posts, setPosts] = useState([]);
   const [isAnonymous, setIsAnonymous] = useState(false);
 
+  const [showModal, setShowModal] = useState(false);
+
   const getCurrentUrlLastSegment = () => {
     const currentUrl = window.location.href;
     const urlSegments = currentUrl.split("/");
@@ -33,42 +35,6 @@ export default function Post({ getPosts }) {
   const handleDocFileChange = (event) => {
     setDocuments([...event.target.files]);
   };
-
-  // const handleInputChange = (e) => {
-  //   const { name, value } = e.target;
-  //   if (name === "caption") {
-  //     setCaption(value);
-  //   } else if (name === "description") {
-  //     setDescription(value);
-  //   }
-  // };
-
-  // const handleUpload = async () => {
-  //   try {
-  //     const formData = new FormData();
-  //     // formData.append("caption", caption);
-  //     // formData.append("description", description);
-  //     formData.append("event", event_id);
-  //     formData.append("image", images);
-  //     formData.append("file", documents);
-  //     formData.append("is_anonymous", isAnonymous.toString());
-  //     console.log("form nè >", formData);
-  //     await apiClient({
-  //       method: "post",
-  //       url: "http://192.168.1.167:5000/add_post",
-  //       data: formData,
-  //       headers: { "Content-Type": "multipart/form-data" },
-  //     });
-  //     toast.success("Add Post Success");
-  //     getPosts();
-  //     handleClose();
-  //   } catch (error) {
-  //     toast.error(error.response.data.message);
-  //     console.log(error);
-  //   }
-
-  //   handleClose();
-  // };
 
   const handleUpload = async () => {
     const formData = new FormData();
@@ -112,6 +78,10 @@ export default function Post({ getPosts }) {
     resetForm(); // Reset form khi đóng modal
 
     setShow(false);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   const resetForm = () => {
@@ -188,11 +158,68 @@ export default function Post({ getPosts }) {
             <Form.Group controlId="formGridCheckbox">
               <Form.Check
                 type="checkbox"
-                label="I agree to the terms and conditions"
+                label={
+                  <span
+                    onClick={toggleModal}
+                    style={{
+                      cursor: "pointer",
+                      color: "blue",
+                      textDecoration: "underline",
+                    }}
+                  >
+                    I agree to the terms and conditions
+                  </span>
+                }
                 checked={isChecked}
                 onChange={handleCheckboxChange}
               />
             </Form.Group>
+
+            <Modal show={showModal} onHide={toggleModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>Terms and Conditions</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <b>
+                  Welcome to GreenWich University's online blog. This blog is a
+                  platform that allows students to share articles, opinions and
+                  information related to school events. By using this blog, you
+                  agree to comply with the following terms and conditions:
+                </b>
+                <br />
+                <br />
+                <p>
+                  <b>1: Scope of Contribution :</b> You may post articles,
+                  comments and images related to school-organized events or
+                  student-related activities. All posted content must be
+                  appropriate and respect copyright and privacy regulations and
+                  not violate current laws.
+                  <br />
+                  <b>2: Content Policy :</b> Avoid posting posts or comments
+                  that are offensive, discriminatory, insulting, or may harm
+                  others. The University reserves the right to remove any
+                  content determined to be inappropriate without prior notice.
+                  <br />
+                  <b>3: Personal Responsibility :</b> You are solely responsible
+                  for the content you post. The University is not responsible
+                  for any claims arising from content provided by you.
+                  <br />
+                  <b>4: Security and Privacy : </b>
+                  Respect the privacy of others. Do not post other people's
+                  personal information without their consent.
+                  <br />
+                  <b>5: Changes to Terms and Conditions : </b>
+                  The University reserves the right to amend these terms and
+                  conditions at any time. Changes will be announced on the blog
+                  and will take effect immediately after being posted.
+                </p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={toggleModal}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Form>
         </Modal.Body>
         <Modal.Footer>
